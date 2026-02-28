ebc9a895c08c209f2d188762b1f45907d1b695f7
Tahina <92741906+Tahina1@users.noreply.github.com>
2026-02-28 22:08:50 +0300

    feat: refactor user routes

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ connectDB().catch((err) => console.error(err));
 //app.use(express.json());
 
 // Routes
-app.use("/api/users", authorize("CLIENT"), userRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes)
 app.use("/api/shops", shopRoutes);
 app.use("/api/product-categories", productCategoryRoutes);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const userController = require("../controllers/user.controller");
+const authenticateToken = require("../middlewares/auth.middleware");
+const authorize = require("../middlewares/authorize.middleware");
 
 const router = express.Router();
 
 // route to get all users
-router.get("/", userController.getAllUsers);
+router.get("/", authenticateToken,  authorize("SHOP", "ADMIN"), userController.getAllUsers);
 
 // route to create a new user
-router.post("/", userController.createUser);
+router.post("/", authenticateToken, authorize("SHOP", "ADMIN"),  userController.createUser);
 
 module.exports = router;

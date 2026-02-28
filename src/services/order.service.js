7383b168551004d6db88e11baf4be9f6d75aa776
Tahina <92741906+Tahina1@users.noreply.github.com>
2026-02-28 18:48:39 +0300

    feat: add snapshot name

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -5,30 +5,41 @@ const Shop = require("../models/shop.model");
 const AppError = require("../utils/AppError");
 const Order = require("../models/order.model");
 
-exports.processCheckout = async (ownerId, shippingAddress) => {
+exports.processCheckout = async (ownerId, billingDetails, shippingAddress) => {
     if(!shippingAddress) throw new AppError("Shipping address is required", 400);
     const session = await mongoose.startSession();
     session.startTransaction();
     try {
-        const cart = await Cart.findOne({ ownerId }).populate("items.productId").session(session);
+        const cart = await Cart.findOne({ ownerId })
+        .populate("items.productId")
+        .populate("items.shopId")
+        .session(session);
         //Validate globaly Cart
         if(!cart) throw new AppError("Cart not found", 404);
         if(cart.items.length === 0) throw new AppError("Cart is empty", 400);
 
         //Validate product and shop
         const groupedByOrder = cart.items.reduce((groups, item) => {
-            const shopIdAsKey = item.shopId.toString();
+            const shopIdAsKey = item.shopId._id.toString();
             if(!groups[shopIdAsKey]) groups[shopIdAsKey] = [];
             groups[shopIdAsKey].push(item); 
             return groups;
 
         },{}); //map
 
-        for(const shop in groupedByOrder){
-            const shopItems = groupedByOrder[shop];
+        for(const shopIdAsKey in groupedByOrder){
+            const shopItems = groupedByOrder[shopIdAsKey];
             const order = new Order({
                 customerId: ownerId,
-                shopId: shop,
+                billingDetails: billingDetails,
+                shopId: shopIdAsKey,
+                // as we group by shop, 
+                // all items have the same shop, 
+                // so we can take the name from the first item
+                // we have already populated shop in cart for each item, 
+                // so we can access shop name without extra query
+                // so for each first item we take shop name and use it for whole order
+                shopName: shopItems[0].shopId.name, 
                 items: [],
                 totalAmount: 0,
                 shippingAddress: shippingAddress
@@ -44,7 +55,8 @@ exports.processCheckout = async (ownerId, shippingAddress) => {
                 }
 
                 order.items.push({
-                    productId: item.productId,
+                    productId: item.productId._id,
+                    productName: item.productId.name,
                     quantity: item.quantity,
                     priceAtOrder: item.priceAtAdd
                 })
@@ -53,6 +65,7 @@ exports.processCheckout = async (ownerId, shippingAddress) => {
                 await product.save({ session });              
                 order.totalAmount += item.quantity * item.priceAtAdd;
             }
+            order.status = "Processing";
             await order.save({ session });
         }
         await Cart.deleteOne({ ownerId }).session(session);

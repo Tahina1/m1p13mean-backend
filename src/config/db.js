eb590a3759ac9d7c623731f7b4175e77db2aa832
Tahina <92741906+Tahina1@users.noreply.github.com>
2026-03-01 12:33:36 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://admin:root@clusterecommerce.grqptpf.mongodb.net/m1p13mean_miangaly_tahina_dev?appName=ClusterEcommerce");//process.env.MONGO_URI
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);

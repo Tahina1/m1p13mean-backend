fd7bb0050c73ba0e4e5e0f43e86d5b50daf2404e
Tahina <92741906+Tahina1@users.noreply.github.com>
2026-03-02 23:12:54 +0300

    feat: add get product by id

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -5,12 +5,12 @@ const authorize = require('../middlewares/authorize.middleware');
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 const { uploadToVercelBlob } = require('../middlewares/vercel-upload.middleware');
-const { validateCreateProduct, validateGetProducts, validatePatchProduct } = require('../validators/product.validator');
+const { validateCreateProduct, validateGetProducts, validatePatchProduct, validateGetProductById } = require('../validators/product.validator');
 
 // POST /api/products - only accessible for admins and shops
-router.post('/', 
-    authenticate, 
-    authorize("ADMIN", "SHOP"), 
+router.post('/',
+    authenticate,
+    authorize("ADMIN", "SHOP"),
     upload.array("images"),
     uploadToVercelBlob("images"),
     validateCreateProduct,
@@ -27,8 +27,13 @@ router.patch('/:id',
 )
 
 // GET /api/products - accessible for all users
-router.get('/', 
+router.get('/',
     validateGetProducts,
     productController.getProducts);
+
+// GET /api/products/:id - accessible for all users
+router.get('/:id',
+    validateGetProductById,
+    productController.getProductById);
 
 module.exports = router;
